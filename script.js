26809eb45fcd9fa6ad2fd6e3d55ada9ec228b895
Your Name <you@example.com>
2025-01-10 10:39:08 +0100

    API progresse

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const title = document.getElementById('main-title');
-    setTimeout(function() {
-      title.classList.add('show');
-    }, 1000); // Délai de 1 seconde avant que l'animation ne commence
-  });
-  
+  alerte("coucou");
+  var ladivmagique = document.getElementById("idListMedecin");
