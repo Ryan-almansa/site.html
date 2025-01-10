17e1cc7d2294d25dc6284bf00da505f3c5d704cc
Your Name <you@example.com>
2025-01-10 10:58:13 +0100

    commit js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
   alerte("chargement");
   var ladivmagique = document.getElementById("idListMedecin");
+
+
+  fetch('http://192.168.65.113:8080/Route1', {
+    headers: {
+        Accept: 'application/json'
+    }
+}).then(response => {
+    if (response.ok) {
+        return response.json()
+    } else {
+        throw new Error('Erreur serveur', { cause: response })
+    }
+}).then(result => {
+    console.log('La liste des articles : ', result)
+}).catch(error => {
+    console.error('Une erreur est survenue', error)
+})
